fd66b78fd14ca03749db87ef2506c68f0fe61c9a
Joe <24jdburks@macon.k12.mo.us>
2021-10-14 10:52:43 -0500

    sixth push

---
+++ ShoppingCart01.java
@@ -0,0 +1,20 @@
+
+package shoppingcart01;
+
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+      
+// Shopping Cart by Joe Burks 10/14/21  
+
+// Declare and initialize String variables.  Do not initialize message yet.
+        String custName = "Joe ";
+        String itemDesc = "shirt ";
+        String message = custName + "wants to purchase a" + itemDesc;
+        
+        // Assign the message variable 
+        System.out.println(message);
+        
+        // Print and run the code
+        
+    }
+}
