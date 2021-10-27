c1990efb325ddaa53a55954df2c3fc193e688aff
Ethan <24jdburks@macon.k12.mo.us>
2021-10-27 10:41:32 -0500

    thirteenth push

---
+++ Input03.java
@@ -0,0 +1,19 @@
+package input03;
+import java.util.Scanner;
+class Input03 {
+
+    public static void main(String[] args) {
+        //Create a Scanner
+        Scanner sc = new Scanner(System.in);
+        //Find and print the sum of three integers entered by the user
+        int x = 2;
+        int y = 3;
+       
+        System.out.println(x + y);
+        
+        System.out.println("hello");
+        
+        //Remember to close the Scanner
+        sc.close();
+    }
+}
